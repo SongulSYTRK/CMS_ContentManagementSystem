6ed3dd489659799fb5d8e2a28e1ff144b7a0df65
songul soyturk <songulsytrk@gmail.com>
2021-12-06 21:59:58 +0300

    Categories added component

---
+++ source/Web/CMS.Presentation/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using CMS.Application.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Presentation.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            var product = await _productService.GetProduct();
+            return View(product);
+        }
+    }
+}
---
+++ source/Web/CMS.Presentation/Models/Components/CategoriesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using CMS.Application.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Presentation.Models.Components
+{
+    public class CategoriesViewComponent:ViewComponent 
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesViewComponent(ICategoryService categoryService) => _categoryService = categoryService;
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _categoryService.GetCategory());
+    }
+}
